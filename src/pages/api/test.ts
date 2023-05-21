404bb856aa59429a992fce3fd901471c3aa86e27
Brendan Brown <brendan@arcadiapower.com>
2023-05-21 11:05:40 -0700

    test route 11

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -3,26 +3,22 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import path from 'path';
 import fs from 'fs';
 
-const dbPath = path.join(process.cwd(), 'database/db.sqlite');
+// const dbPath = path.join(process.cwd(), 'database/db.sqlite');
 
-console.log(
-  '******* CWD ',
-  fs.readdirSync(path.join(process.cwd(), 'database'))
-);
+// const db = new Database(dbPath, {
+//   readonly: false,
+//   timeout: 5000,
+//   // verbose: console.log,
+// });
 
-const db = new Database(dbPath, {
-  readonly: false,
-  timeout: 5000,
-  // verbose: console.log,
-});
-
-const getMovieById = db.prepare<number>(`
-  SELECT title FROM movies WHERE id = ?;
-`);
+// const getMovieById = db.prepare<number>(`
+//   SELECT title FROM movies WHERE id = ?;
+// `);
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const response = getMovieById.get(1);
-  res.status(200).json(response);
+  // const response = getMovieById.get(1);
+  const files = fs.readdirSync(path.join(process.cwd(), 'database'));
+  res.status(200).json(files);
 };
 
 export default handler;
