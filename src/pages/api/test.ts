f27934a3b551ee14de9a65084c9446c817d77519
Brendan Brown <brendan@arcadiapower.com>
2023-05-21 09:56:49 -0700

    test route 9

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,8 +1,13 @@
 import Database from 'better-sqlite3';
 import { NextApiRequest, NextApiResponse } from 'next';
 import path from 'path';
+import fs from 'fs';
 
 const dbPath = path.join(process.cwd(), 'database/db.sqlite');
+
+console.log('******* DIRNAME ', fs.readdirSync(__dirname));
+console.log('******* CWD ', fs.readdirSync(process.cwd()));
+console.log('******* DBPATH', dbPath);
 
 const db = new Database(dbPath, {
   readonly: false,
