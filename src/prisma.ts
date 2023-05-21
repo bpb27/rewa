d7e427e2f54c71db51d28c3902f93ea14a10d0c9
Brendan Brown <brendan@arcadiapower.com>
2023-05-21 15:47:19 -0700

    good god it broke again

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,4 +1,9 @@
+import fs from 'fs';
+import path from 'path';
 import { PrismaClient } from '@prisma/client';
+
+// needed so vercel adds prisma dir to serveless api lambdas
+const files = fs.readdirSync(path.join(process.cwd(), 'prisma'));
 
 export class Prisma {
   public static Prisma: PrismaClient;
