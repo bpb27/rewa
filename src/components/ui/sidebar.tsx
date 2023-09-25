ca52a9060ea893658b88e8b131528cba2186c01e
Brendan Brown <brendan@arcadiapower.com>
2023-09-24 21:55:38 -0700

    little sidebar animation

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -4,7 +4,7 @@ import { MovieCardPoster, TheaterBackground } from '../images';
 import { Icon } from '../icons';
 
 export const Sidebar = ({ children }: PropsWithChildren<{}>) => (
-  <div className="fixed right-0 top-8 z-10 mb-4 h-full w-3/4 overflow-y-scroll border-l-4 bg-slate-100 p-5 pb-8 text-center md:w-1/2 lg:w-1/4">
+  <div className="fixed right-0 top-8 z-10 mb-4 h-full w-3/4 animate-enterFromRight overflow-y-scroll border-l-4 bg-slate-100 p-5 pb-8 text-center md:w-1/2 lg:w-1/4">
     {children}
   </div>
 );
