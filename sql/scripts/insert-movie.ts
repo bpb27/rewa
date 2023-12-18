10e3be0d892bd34cf874815c5f75e62c9007e0e1
Brendan Brown <brendan@arcadiapower.com>
2023-12-18 14:30:45 -0800

    data update 12/18/2023

--- sql/scripts/insert-movie.ts
+++ sql/scripts/insert-movie.ts
@@ -2,7 +2,6 @@ import { connectToDb } from '../general';
 import { insertNewEbert, insertNewEpisode, insertNewMovie } from '../insert';
 import { tmdbApi } from '../tmdb-api';
 
-// TODO: add keywords to getMovieById and insertNewMovie
 const run = async (params: {
   tmdb_id: number;
   title: string;
@@ -25,32 +24,14 @@ const run = async (params: {
 
 const newEps = [
   {
-    tmdb_id: 12150,
-    title: 'Sea of Love',
-    episode_order: 323,
+    tmdb_id: 9944,
+    title: 'The Pelican Brief',
+    episode_order: 324,
     date: 'Dec 2023',
-    spotify_url: 'https://open.spotify.com/episode/0h6l5Si3mLsQg80vyfGVU3?si=9d6952c59a0c422f',
-    hosts: ['Bill Simmons', 'Chris Ryan', 'Wosny Lambre'],
-    ebert: { rating: 3, path: '/reviews/sea-of-love-1989' },
-  },
-  {
-    tmdb_id: 2105,
-    title: 'American Pie',
-    episode_order: 322,
-    date: 'Nov 2023',
-    spotify_url: 'https://open.spotify.com/episode/7kOnZui462foKfj6eIHhfH?si=8e34bee053f24896',
-    hosts: ['Bill Simmons', 'Chris Ryan', 'Sean Fennessey'],
-    ebert: { rating: 3, path: '/reviews/american-pie-1999' },
-  },
-  {
-    tmdb_id: 787,
-    title: 'Mr. and Mrs. Smith',
-    episode_order: 321,
-    date: 'Nov 2023',
-    spotify_url: 'https://open.spotify.com/episode/6HXn5A4fybXTe5cRdU3ta8?si=38637b3320ce473b',
-    hosts: ['Bill Simmons', 'Chris Ryan', 'Amanda Dobbins'],
-    ebert: { rating: 3, path: '/reviews/mr-and-mrs-smith-2005' },
+    spotify_url: 'https://open.spotify.com/episode/6Bq0AFIKEgmfGRtLLWFJwn?si=7981ee3e95034232',
+    hosts: ['Bill Simmons', 'Chris Ryan', 'Sean Fennessey', 'Amanda Dobbins'],
+    ebert: { rating: 3, path: '/reviews/the-pelican-brief-1993' },
   },
 ];
 
-run(newEps[2]);
+run(newEps[0]);
