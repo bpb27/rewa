d2e6ef03ab3c2036f4b4545177d614f6a5793d04
Brendan Brown <brendan@arcadiapower.com>
2024-01-12 22:04:53 -0800

    add fs call for prisma dir

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,7 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { getOscarMovies } from '~/api/get-oscar-movies';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const movies = await getOscarMovies();
-  res.status(200).json({ total: movies.length, movies });
+  res.status(200).json({ name: 'Yay' });
 }
--- src/prisma.ts
+++ src/prisma.ts
@@ -1,4 +1,9 @@
 import { PrismaClient } from '@prisma/client';
+import fs from 'fs';
+import path from 'path';
+
+// needed so vercel adds prisma dir to serveless api lambdas
+const files = fs.readdirSync(path.join(process.cwd(), 'prisma'));
 
 // https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices#problem
 const prismaClientSingleton = () => {
