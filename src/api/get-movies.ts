3d32c196cc53a4b300711901e4947769d8eec466
Brendan Brown <brendan@arcadiapower.com>
2024-01-13 21:16:08 -0800

    optimize getMovies by ditching initial sync filter call and fetching count in parallel

--- src/api/get-movies.ts
+++ src/api/get-movies.ts
@@ -19,8 +19,6 @@ import { sortCrew } from '~/utils/sorting';
 // NB: using a view (movies_with_computed_fields) to sort across tables (e.g. episode order) + custom fields (e.g. profit percentage)
 
 type QueryWhere = Pick<PrismaBaseType.moviesFindManyArgs, 'where'>;
-type QueryOrderBy = Pick<PrismaBaseType.movies_with_computed_fieldsFindManyArgs, 'orderBy'>;
-type QuerySkip = Pick<PrismaBaseType.movies_with_computed_fieldsFindManyArgs, 'skip'>;
 type OrderByKey = keyof PrismaBaseType.movies_with_computed_fieldsOrderByWithAggregationInput;
 
 const prisma = Prisma.getPrisma();
@@ -48,99 +46,81 @@ export const getMovies = async (params: QpSchema) => {
   const searches = getSearches(params);
   const prismaSearch = searches.length ? { [mode]: createFilters(mode, searches) } : undefined;
 
-  const where: QueryWhere = {
-    where: {
-      ...(params.movieMode === 'rewa' ? { episodes: { some: {} } } : undefined),
-      ...(params.movieMode === 'oscar' ? { oscars_nominations: { some: {} } } : undefined),
-      ...prismaSearch,
-    },
+  const where: QueryWhere['where'] = {
+    ...(params.movieMode === 'rewa' ? { episodes: { some: {} } } : undefined),
+    ...(params.movieMode === 'oscar' ? { oscars_nominations: { some: {} } } : undefined),
+    ...prismaSearch,
   };
 
-  const orderBy: QueryOrderBy = {
-    orderBy: { [sortMap[params.sort]]: sortOrder },
-  };
-
-  const skip: QuerySkip = {
-    skip: offset,
-  };
-
-  const [total, matchedIds] = await Promise.all([
-    prisma.movies.count({ ...where }),
+  const [total, data] = await Promise.all([
+    prisma.movies.count({ where }),
     prisma.movies_with_computed_fields.findMany({
-      select: { movie_id: true },
-      where: { movie: where.where },
+      where: { movie: where },
+      orderBy: { [sortMap[params.sort]]: sortOrder },
       take,
-      ...skip,
-      ...orderBy,
-    }),
-  ]);
-
-  const data = await prisma.movies_with_computed_fields.findMany({
-    ...orderBy,
-    where: {
-      movie_id: { in: matchedIds.map(match => match.movie_id) },
-    },
-    select: {
-      movie: {
-        select: {
-          budget: true,
-          id: true,
-          imdb_id: true,
-          overview: true,
-          poster_path: true,
-          release_date: true,
-          revenue: true, // NB: stored in DB as / 1000 due to BigInt shit
-          runtime: true,
-          title: true,
-          tagline: true,
-          actors_on_movies: {
-            orderBy: { credit_order: 'asc' },
-            select: { actors: { select: { id: true, name: true, profile_path: true } } },
-          },
-          crew_on_movies: {
-            where: {
-              job: {
-                in: Object.values(crewJobs).flat(),
+      skip: offset,
+      select: {
+        movie: {
+          select: {
+            budget: true,
+            id: true,
+            imdb_id: true,
+            overview: true,
+            poster_path: true,
+            release_date: true,
+            revenue: true, // NB: stored in DB as / 1000 due to BigInt shit
+            runtime: true,
+            title: true,
+            tagline: true,
+            actors_on_movies: {
+              orderBy: { credit_order: 'asc' },
+              select: { actors: { select: { id: true, name: true, profile_path: true } } },
+            },
+            crew_on_movies: {
+              where: {
+                job: {
+                  in: Object.values(crewJobs).flat(),
+                },
+              },
+              select: {
+                job: true,
+                crew: selectIdAndName,
               },
             },
-            select: {
-              job: true,
-              crew: selectIdAndName,
+            ebert_reviews: true,
+            episodes: {
+              take: 1,
+              select: {
+                id: true,
+                spotify_url: true,
+                episode_order: true,
+                hosts_on_episodes: { select: { hosts: selectIdAndName } },
+              },
             },
-          },
-          ebert_reviews: true,
-          episodes: {
-            take: 1,
-            select: {
-              id: true,
-              spotify_url: true,
-              episode_order: true,
-              hosts_on_episodes: { select: { hosts: selectIdAndName } },
+            genres_on_movies: {
+              select: { genres: selectIdAndName },
             },
-          },
-          genres_on_movies: {
-            select: { genres: selectIdAndName },
-          },
-          keywords_on_movies: {
-            select: { keywords: selectIdAndName },
-          },
-          oscars_nominations: {
-            select: {
-              recipient: true,
-              won: true,
-              ceremony_year: true,
-              award: { select: { oscars_categories: selectIdAndName } },
+            keywords_on_movies: {
+              select: { keywords: selectIdAndName },
             },
-          },
-          streamers_on_movies: {
-            select: { streamers: selectIdAndName },
+            oscars_nominations: {
+              select: {
+                recipient: true,
+                won: true,
+                ceremony_year: true,
+                award: { select: { oscars_categories: selectIdAndName } },
+              },
+            },
+            streamers_on_movies: {
+              select: { streamers: selectIdAndName },
+            },
           },
         },
+        total_oscar_nominations: true,
+        total_oscar_wins: true,
       },
-      total_oscar_nominations: true,
-      total_oscar_wins: true,
-    },
-  });
+    }),
+  ]);
 
   const movies = data.map(item => {
     const movie = item.movie!;
