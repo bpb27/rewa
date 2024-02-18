f79f16430a81e0ff11327dc8d4daabbf8481ea1e
Brendan Brown <brendanbrown27@gmail.com>
2024-02-18 14:52:04 -0800

    turso int

--- prisma/kysley.ts
+++ prisma/kysley.ts
@@ -1,23 +1,41 @@
+import { LibsqlDialect } from '@libsql/kysely-libsql';
 import Database from 'better-sqlite3';
-import fs from 'fs';
 import { Kysely, ParseJSONResultsPlugin, SqliteDialect } from 'kysely';
-import path from 'path';
 import { type DB } from './generated/types';
 
-// needed so vercel includes sqlite file in the lambas
-const _files = fs.readdirSync(path.join(process.cwd(), 'prisma'));
+// NB: node_modules/kysely/dist/esm/plugin/parse-json-results/parse-json-results-plugin.js
+/*
+  function parseObject(obj) {
+    const newObj = {};
+    for (const key in obj) {
+        newObj[key] = parse(obj[key]);
+    }
+    return newObj;
+}
+*/
 
-const dialect = new SqliteDialect({
+/*
+  surprisingly slow against turso
+  confirmed indexes are all present
+*/
+
+const local = new SqliteDialect({
   database: new Database('./prisma/db.sqlite', {
     readonly: false,
     timeout: 5000,
   }),
 });
 
+const remote = new LibsqlDialect({
+  url: 'libsql://rewa-test-2ky-bpb27.turso.io',
+  authToken:
+    'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDgxODc3ODAsImlkIjoiODYwMzRhNGItY2RiMi0xMWVlLThmMWItNmU5NzQxNjJlYjhkIn0.AvxQBDiceeXJCGkFvnU9WinHjNa4d5s5vg7K5XdlFzs_r0Zx2O-6VGsis2OJnuodUYgU76piT9mH_9PvKe4xCw',
+});
+
 export type KyselyDB = DB;
 
 export const kyselyDb = new Kysely<DB>({
-  dialect,
-  // log: ['query'],
+  dialect: local,
+  log: ['query'],
   plugins: [new ParseJSONResultsPlugin()],
 });
--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,20 +1,13 @@
-// import fs from 'fs';
 import type { NextApiRequest, NextApiResponse } from 'next';
-// import path from 'path';
-import { getOscarsByYear } from '~/apik/get-oscars-by-year';
+import { getMovies } from '~/apik/get-movies';
+import { defaultQps } from '~/data/query-params';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // try {
-  const response = await getOscarsByYear({ year: 1999 });
+  const response = await getMovies({
+    ...defaultQps,
+    sort: 'release_date',
+    keyword: [249],
+    movieMode: 'oscar',
+  });
   res.status(200).json(response);
-  // } catch (e1) {
-  //   try {
-  //     const currentDir = fs.readdirSync(path.join(process.cwd(), 'prisma'));
-  //     const upOneDir = fs.readdirSync(path.join('../', process.cwd()));
-  //     const upTwoDir = fs.readdirSync(path.join('../../', process.cwd()));
-  //     res.status(200).json({ error: JSON.stringify(e1), currentDir, upOneDir, upTwoDir });
-  //   } catch (e2) {
-  //     res.status(200).json({ errorOne: JSON.stringify(e1), errorTwo: JSON.stringify(e2) });
-  //   }
-  // }
 }
