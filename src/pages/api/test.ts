6a3e794c3c5729d14a1a7791c2f7a3808f939504
Brendan Brown <brendanbrown27@gmail.com>
2024-02-27 15:36:05 -0800

    test func 1

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,7 +1,16 @@
+import fs from 'fs';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import path from 'path';
 import { getOscarsByYear } from '~/apik/get-oscars-by-year';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const response = await getOscarsByYear({ year: 1999 });
-  res.status(200).json(response);
+  const currentDir = fs.readdirSync(path.join(process.cwd(), 'prisma'));
+  const upOneDir = fs.readdirSync(path.join('..', process.cwd()));
+  const upTwoDir = fs.readdirSync(path.join('..', process.cwd()));
+  try {
+    const response = await getOscarsByYear({ year: 1999 });
+    res.status(200).json(response);
+  } catch (e) {
+    res.status(500).json({ error: JSON.stringify(e), currentDir, upOneDir, upTwoDir });
+  }
 }
