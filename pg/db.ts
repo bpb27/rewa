3968a7b71b1a6b5f55a407e88a0756630a2e1ad3
Brendan Brown <brendanbrown27@gmail.com>
2024-03-10 08:45:30 -0700

    vercel pg

--- pg/db.ts
+++ pg/db.ts
@@ -1,16 +1,20 @@
+import { createKysely } from '@vercel/postgres-kysely';
 import { Kysely, ParseJSONResultsPlugin, PostgresDialect } from 'kysely';
 import { Pool } from 'pg';
 import { type DB } from './generated';
 
-const dialect = new PostgresDialect({
-  pool: new Pool({
-    connectionString: process.env.DATABASE_URL,
-    max: 10,
-  }),
-});
+const pgConfig = {
+  connectionString: process.env.DATABASE_URL,
+  max: 10,
+};
 
-export const kyselyDb = new Kysely<DB>({
-  dialect,
-  log: ['query'],
-  plugins: [new ParseJSONResultsPlugin()],
-});
+const plugins = [new ParseJSONResultsPlugin()];
+
+export const kyselyDb =
+  process.env.NODE_ENV === 'production'
+    ? createKysely<DB>(pgConfig, { plugins })
+    : new Kysely<DB>({
+        dialect: new PostgresDialect({ pool: new Pool(pgConfig) }),
+        log: ['query'],
+        plugins,
+      });
