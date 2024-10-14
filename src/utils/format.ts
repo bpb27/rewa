d55f69af01ac53a532d026959990ababe5535683
Brendan Brown <brendanbrown27@gmail.com>
2024-10-13 22:03:15 -0700

    fix date formatter

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -25,10 +25,7 @@ export const newFormatDate = (d: number | string | Date, f: 'year' | 'slash' | '
     day: '',
   };
   if (isDate(d)) {
-    components.year = d.getFullYear().toString();
-    components.month =
-      d.getMonth() < 10 ? `${0}${d.getMonth() + 1}` : (d.getMonth() + 1).toString();
-    components.day = d.getDate() < 10 ? `${0}${d.getDate() || 1}` : d.getDate().toString();
+    [components.year, components.month, components.day] = d.toISOString().split('T')[0].split('-');
   } else if (isNumber(d) && isYear(d.toString())) {
     components.year = d.toString();
     components.month = '01';
@@ -37,7 +34,7 @@ export const newFormatDate = (d: number | string | Date, f: 'year' | 'slash' | '
     components.year = d;
     components.month = '01';
     components.day = '01';
-  } else if (isString(d) && [9, 9, 10].includes(d.length) && d.includes('-')) {
+  } else if (isString(d) && [9, 10].includes(d.length) && d.includes('-')) {
     [components.year, components.month, components.day] = d.split('-');
   } else {
     console.error(`Unsupported date input ${d}`);
